afa717974738bf59f7df6b850ce7ea937c0315b2
Fabien LOISON <flo@flogisoft.com>
2022-12-17 13:10:08 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+rst2gemtext - Converts reStructuredText to Gemtext (Gemini markup format)
+=========================================================================
+
+rst2gemtext aims to convert **reStructuredText** markup to **Gemtext**, the markup format used by Gemini. It can be used both as a **Python library** or as a **CLI tool**.
+
+**This project is currently work in progress.**
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+import os
+
+from setuptools import setup, find_packages
+
+
+long_description = ""
+if os.path.isfile("README.rst"):
+    long_description = open("README.rst", "r", encoding="UTF-8").read()
+
+
+setup(
+    name="rst2gemtext",
+    version="0.0.0",
+    description="Converts reStructuredText to Gemtext (Gemini markup format)",
+    url="https://github.com/flozz/rst2gemtext",
+    project_urls={
+        "Source Code": "https://github.com/flozz/rst2gemtext",
+        "Issues": "https://github.com/flozz/rst2gemtext/issues",
+        "Chat": "https://discord.gg/P77sWhuSs4",
+    },
+    license="GPLv3",
+    long_description=long_description,
+    keywords="restructuredtext rst convert gemtext gemini docutils",
+    author="Fabien LOISON",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "docutils"
+    ],
+    extras_require={
+        "dev": [
+            "nox",
+            "flake8",
+            "pytest",
+            "black",
+        ]
+    },
+)
